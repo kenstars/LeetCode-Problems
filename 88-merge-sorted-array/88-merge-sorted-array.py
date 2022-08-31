33fde7786f15b26a5bcc5de4950d8e449aa19170
Kannan Piedy <kannanpiedy@gmail.com>
2022-08-31 19:35:34 +0530

    Time: 62 ms (43.00%), Space: 14.1 MB (5.89%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -5,7 +5,7 @@ class Solution:
         """
         i = j = 0
         total_length = m+n
-        while i<m and j<len(nums2):
+        while i<m and j<n:
             print(i,j)
             if nums1[i] > nums2[j]:
                 nums1.insert(i, nums2[j])
